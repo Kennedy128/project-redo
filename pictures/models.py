c1207c98e7899827114f014e28170fe2d264c3a4
Santa claus <santa@northpole.com>
2020-05-25 21:50:44 +0300

    creation of our models to store our category,pictures and location

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,85 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    '''
+    category class to define category objects
+    '''
+    category = models.CharField(max_length=30)
+
+    def save_category(self):
+        '''
+        method that saves category to database
+        '''
+        self.save()
+    
+    def __str__(self):
+        return self.category
+
+class Location(models.Model):
+    '''
+    location class to define location objects
+    '''
+    location = models.CharField(max_length=30)
+
+    def save_location(self):
+        '''
+        method that saves location to database
+        '''
+        self.save()
+
+    def __str__(self):
+        return self.location
+        
+class Image(models.Model):
+    '''
+    image class to define image objects
+    '''
+    name= models.CharField(max_length = 30)
+    picture= models.ImageField(upload_to = 'images/')
+    description= models.CharField(max_length = 100)
+    category= models.ForeignKey(Category,on_delete=models.CASCADE)
+    location= models.ForeignKey(Location,on_delete=models.CASCADE)
+ 
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        '''
+        method that saves image to database
+        '''
+        self.save()
+
+    @classmethod
+    def display_images(cls):
+        '''
+        method that shows all images in the database
+        '''
+        pictures=cls.objects.all()
+        return pictures
+
+    @classmethod
+    def delete_image(cls,id):
+        '''
+        method that deletes image from database
+        '''
+        cls.objects.filter(id=id).delete()
+
+    @classmethod
+    def search_image(cls,search_term):
+        '''
+        method that retrieves images based on their category
+        '''
+        pictures = cls.objects.filter(category__category__icontains=search_term)
+        return pictures   
+
+    @classmethod
+    def filter_by_location(cls,search_term):
+        '''
+        method that rerieves images based on the location they were taken
+        '''
+        pictures = cls.objects.filter(location__location__icontains=search_term)
+        return pictures   
+
+
+
