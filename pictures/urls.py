1003fc6ca7ae82a44a317a4b63bbe660e85f758e
Santa claus <santa@northpole.com>
2020-05-25 21:46:07 +0300

    adding of our dynamic route to display pictures

---
+++ pictures/urls.py
@@ -0,0 +1,15 @@
+ 
+from django.conf.urls import url
+from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+#urls
+urlpatterns=[
+    url(r'^$',views.allPictures,name = 'allPictures'),
+    url(r'^search/',views.search_pictures,name = 'search_pictures'),
+    url(r'^filterlocation/',views.filter_location,name='filter_location')
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
